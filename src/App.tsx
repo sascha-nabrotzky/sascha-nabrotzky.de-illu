4cc48f5e15f7f780998ebec10aed182289af89ae
Sascha Nabrotzky <sascha.nabrotzky@mywellness.de>
2023-02-19 12:09:35 +0100

    Add more tailwind classes and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,29 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App grid">
-      <header className="flex p-4">
+    <div className="App grid h-screen">
+      <header className="flex p-4 mb-8">
         <div className="types flex flex-wrap justify-between">
-          <h2 className="letter font-sans mx-12">S</h2>
-          <h2 className="letter font-sans mx-12">A</h2>
-          <h2 className="letter font-sans mx-12">S</h2>
-          <h2 className="letter font-sans mx-12">C</h2>
-          <h2 className="letter font-sans mx-12">H</h2>
-          <h2 className="letter font-sans mx-12">A</h2>
-          <h2 className="letter font-serif mx-12">N</h2>
-          <h2 className="letter font-serif mx-12">A</h2>
-          <h2 className="letter font-serif mx-12">B</h2>
-          <h2 className="letter font-serif mx-12">R</h2>
-          <h2 className="letter font-serif mx-12">O</h2>
-          <h2 className="letter font-serif mx-12">T</h2>
-          <h2 className="letter font-serif mx-12">Z</h2>
-          <h2 className="letter font-serif mx-12">K</h2>
-          <h2 className="letter font-serif mx-12">Y</h2>
+          <h2 className="text-8xl font-sans mx-12">S</h2>
+          <h2 className="text-8xl font-sans mx-12">A</h2>
+          <h2 className="text-8xl font-sans mx-12">S</h2>
+          <h2 className="text-8xl font-sans mx-12">C</h2>
+          <h2 className="text-8xl font-sans mx-12">H</h2>
+          <h2 className="text-8xl font-sans mx-12">A</h2>
+          <h2 className="text-8xl font-serif mx-12">N</h2>
+          <h2 className="text-8xl font-serif mx-12">A</h2>
+          <h2 className="text-8xl font-serif mx-12">B</h2>
+          <h2 className="text-8xl font-serif mx-12">R</h2>
+          <h2 className="text-8xl font-serif mx-12">O</h2>
+          <h2 className="text-8xl font-serif mx-12">T</h2>
+          <h2 className="text-8xl font-serif mx-12">Z</h2>
+          <h2 className="text-8xl font-serif mx-12">K</h2>
+          <h2 className="text-8xl font-serif mx-12">Y</h2>
         </div>
       </header>
-      <main className="grid gap-16">
-        <h1 className="absolute bottom-8 opacity-20 tracking-wider break-all font-serif ml-16">Grafikdesign &amp; Illustration</h1>
-        <div className="text-lg tracking-wider">
+      <main className="grid gap-16 p-4 md:p-0">
+        <h1 className="absolute bottom-8 opacity-20 tracking-wider break-all font-serif text-8xl ml-16 transition-filter duration-300 hover:blur">Grafikdesign &amp; Illustration</h1>
+        <div className="md:col-start-2 md:col-span-1 text-lg tracking-wider">
           <h3>Hallo!</h3>
           <p>
             Neben meinem Hauptjob als Webdesigner (UI/UX) habe ich noch
@@ -39,24 +39,24 @@ function App() {
             das UI-Design meiner Websites und Apps.
           </p>
         </div>
-        <div className="image-right flex overflow-hidden place-items-center">
+        <div className="md:col-start-3 md:col-span-2 flex overflow-hidden place-items-center max-h-96">
           <img className="w-full" src={deranfang} alt="Collage" />
         </div>
       </main>
-      <footer className="flex justify-center p-4">
-        <a className="mx-4 hover:opacity-50"
+      <footer className="flex justify-center p-4 z-10">
+        <a className="mx-4 transition-opacity duration-300 hover:opacity-50"
           href="https://sascha-nabrotzky.github.io"
           target="_blank"
           title="Zu meiner GitHub-Website"
         >
-          <img src={globeIcon} />
+          <img className="h-8" src={globeIcon} />
         </a>
-        <a className="mx-4 hover:opacity-50"
+        <a className="mx-4 transition-opacity duration-300 hover:opacity-50"
           href="https://www.instagram.com/sn.de.sign"
           target="_blank"
           title="Zu meinem Instagram-Account"
         >
-          <img src={instaLogo} />
+          <img className="h-8" src={instaLogo} />
         </a>
       </footer>
       <div className="yellow-line bg-yellow-400" />
