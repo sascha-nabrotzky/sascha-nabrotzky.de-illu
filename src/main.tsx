aa18dd1c5c13a3307ccd251799741839521bef99
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2023-03-23 09:31:57 +0100

    working on slider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from "react-dom/client";
 import App from './App'
+import "flowbite";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(<App />);
