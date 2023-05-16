af29638b72b144c7fbba370c2983ae7d476955f2
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2023-05-16 14:33:20 +0200

    add images

--- src/Childrensbooks.tsx
+++ src/Childrensbooks.tsx
@@ -1,42 +1,39 @@
 import React from "react";
 import { Carousel } from "flowbite-react";
-import image1 from "../src/assets/images/1.jpg";
-import image2 from "../src/assets/images/2.jpg";
-import image3 from "../src/assets/images/3.jpg";
-import image4 from "../src/assets/images/4.jpg";
-import image5 from "../src/assets/images/5.jpg";
+import image1 from "../src/assets/images/Book4.jpg";
+import image2 from "../src/assets/images/Book1.jpg";
+import image3 from "../src/assets/images/Book2.jpg";
+import image4 from "../src/assets/images/Book3.jpg";
 
 const Childrensbooks = () => {
   return (
     <>
-      <h1 className="absolute bottom-8 opacity-20 tracking-wider break-all font-serif text-8xl ml-8 md:ml-16 transition-filter duration-300 hover:blur">
+      <h1 className="absolute bottom-8 opacity-20 uppercase tracking-widest break-all font-serif text-8xl ml-8 md:ml-16 transition-filter duration-300 hover:blur">
         Kinderbücher
       </h1>
       <div className="md:col-start-2 md:col-span-1 text-lg tracking-wider">
-        <h2>Kinderbücher</h2>
-        <p>Hier kommt der Text zu meinen Kinderbüchern hin.</p>
+        <h2 className="mb-3">Aus einer Idee entstand eine Zusammenarbeit ...</h2>
+        <p>und jetzt sind schon drei Kinderbücher von mir privat illustriert worden. 
+          Am Anfang stand das Characterdesign der Hauptfiguren und ein Farbschema wurde ausgearbeitet. Die Skizzen wurden eingescannt und digital weiterverarbeitet, um Farben und Hintergründe besser anpassen zu können.</p>
+          <p>Für das vierte Buch, habe ich die drei vorherigen komplett überarbeitet und die verschiedenen Zeichenstile so gut es geht angeglichen. Alle Seiten enthalten doppleseitige Illustrationen.</p>
       </div>
       <div className="md:col-start-3 md:col-span-2 h-96 md:h-72 lg:h-80 xl:h-[30rem] 2xl:h-[38rem]">
         <Carousel>
           <img
             src={image1}
-            alt="..."
+            alt="Mein viertes Kinderbuch"
           />
           <img
             src={image2}
-            alt="..."
+            alt="Mein erstes Kinderbuch"
           />
           <img
             src={image3}
-            alt="..."
+            alt="Mein zweites Kinderbuch"
           />
           <img
             src={image4}
-            alt="..."
-          />
-          <img
-            src={image5}
-            alt="..."
+            alt="Mein drittes Kinderbuch"
           />
         </Carousel>
       </div>
--- src/Illustration.tsx
+++ src/Illustration.tsx
@@ -1,42 +1,49 @@
 import React from "react";
 import { Carousel } from "flowbite-react";
-import image1 from "../src/assets/images/1.jpg";
-import image2 from "../src/assets/images/2.jpg";
-import image3 from "../src/assets/images/3.jpg";
-import image4 from "../src/assets/images/4.jpg";
-import image5 from "../src/assets/images/5.jpg";
+import image1 from "../src/assets/images/illuAlebrije.jpg";
+import image2 from "../src/assets/images/illuAnimals.jpg";
+import image3 from "../src/assets/images/IlluArten.jpg";
+import image4 from "../src/assets/images/illuTheo.jpg";
+import image5 from "../src/assets/images/illuNean.jpg";
+import image6 from "../src/assets/images/illuMorla.jpg";
 
 const Illustration = () => {
   return (
     <>
-      <h1 className="absolute bottom-8 opacity-20 tracking-wider break-all font-serif text-8xl ml-8 md:ml-16 transition-filter duration-300 hover:blur">
+      <h1 className="absolute bottom-8 opacity-20 uppercase tracking-widest break-all font-serif text-8xl ml-8 md:ml-16 transition-filter duration-300 hover:blur">
         Illustration
       </h1>
       <div className="md:col-start-2 md:col-span-1 max-md:px-6 text-lg tracking-wider">
-        <h2>Illustration</h2>
-        <p>Hier kommt der Text zu den Illustrationen hin</p>
+        <h2 className="mb-4">Vieles digital, alles andere aber am Liebsten handgemacht</h2>
+        <p>Hauptsächlich erstelle ich viele Illustrationen für Kinder, aber auch für den Editorial-Bereich habe ich schon einige Sachen gemacht.</p>
+        <p>Grundsätzlich versuche ich den handgemachten, skizzenhaften Look meiner Illustrationen herauszuarbeiten, wenn ich digital arbeite oder erstelle meine diese direkt mit Aquarellstiften und Wasserfarben.</p>
+        <p>Der Reiz der Wasserfarben liegt darin, dass man diese nicht immer so kontrollieren kann und immer etwas unvorhergesehnes passiert.</p>
       </div>
       <div className="md:col-start-3 md:col-span-2 h-96 md:h-72 lg:h-80 xl:h-[30rem] 2xl:h-[38rem]">
         <Carousel>
           <img
             src={image1}
-            alt="..."
+            alt="Illustration Alebrije"
           />
           <img
             src={image2}
-            alt="..."
+            alt="Illustration Tiere"
           />
           <img
             src={image3}
-            alt="..."
+            alt="Illustration Artenvielfalt"
           />
           <img
             src={image4}
-            alt="..."
+            alt="Illustration Hund"
           />
           <img
             src={image5}
-            alt="..."
+            alt="Illustration Neantertalerjunge"
+          />
+          <img
+            src={image6}
+            alt="Illustration Morla"
           />
         </Carousel>
       </div>
