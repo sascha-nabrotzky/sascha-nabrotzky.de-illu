41a8ac476e31b2e876ac83fc45c7f0aed653d4c3
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2023-06-26 16:34:54 +0200

    install compression

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from "vite-plugin-svgr"
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import path from "path";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        svgr()
-    ],
+    plugins: [react(), svgr(), viteCompression()],
     resolve: {
         alias: {
             // damit HG-Bilder beim build mit eingebunden werden
             "~@": path.resolve(__dirname, "/src"),
         },
     },
-})
+});
