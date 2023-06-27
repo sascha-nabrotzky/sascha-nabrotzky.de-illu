6a4d2c40c65879659b54df91ded33629ae6746f0
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2023-06-27 15:02:08 +0200

    remove unused path for flowbite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,6 @@ module.exports = {
     content: [
         "./index.html", 
         "./src/**/*.{js,jsx,ts,tsx}", 
-        "./node_modules/flowbite/**/*.js",
         "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
