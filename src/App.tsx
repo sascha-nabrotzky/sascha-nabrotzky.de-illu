58ce46e87f988948e32a610e9678539fe98e1a72
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2023-06-30 22:48:48 +0200

    move files to component

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,30 @@ import Illustration from "./Illustration";
 import Childrensbooks from "./Childrensbooks";
 import Impressum from "./Impressum";
 import Datenschutz from "./Datenschutz";
-import Footer from "./Footer";
-import Header from "./Header";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 import "./App.css";
 
 const App = () => {
-    return (
-        <Router>
-            <div className="App relative grid h-screen grid-rows-[min-content_1fr_min-content]">
-                <Navigation />
-                <Header />
-                <main className="grid gap-x-16 gap-y-20 md:grid-cols-default text-stone-200">
-                    <Routes>
-                        <Route path="/" element={<Illustration />} />
-                        <Route path="/kinderbuecher" element={<Childrensbooks />} />
-                        <Route path="/ueber-mich" element={<About />} />
-                        <Route path="/impressum" element={<Impressum />} />
-                        <Route path="/datenschutz" element={<Datenschutz />} />
-                    </Routes>
-                </main>
-                <Footer />
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App relative grid h-screen grid-rows-[min-content_1fr_min-content]">
+        <Navigation />
+        <Header />
+        <main className="grid gap-x-16 gap-y-20 md:grid-cols-default text-stone-200">
+          <Routes>
+            <Route path="/" element={<Illustration />} />
+            <Route path="/kinderbuecher" element={<Childrensbooks />} />
+            <Route path="/ueber-mich" element={<About />} />
+            <Route path="/impressum" element={<Impressum />} />
+            <Route path="/datenschutz" element={<Datenschutz />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ReactComponent as Logo } from "../assets/images/logo_sn.svg";
+import colorSplat from "../assets/images/circle.webp";
+
+const Header = () => {
+  return (
+    <header className="relative flex justify-center mt-48 mb-24 md:mt-16 md:mb-28">
+      <Logo className="w-full max-w-md text-stone-200 z-10 pt-10" />
+      <img
+        className="absolute h-60 sm:h-64"
+        src={colorSplat}
+        alt="watercolor splat"
+      />
+    </header>
+  );
+};
+
+export default Header;
