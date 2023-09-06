71165744cc012d5da1bbef4ca076983ef76b5973
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2023-09-06 08:14:17 +0200

    switch to new react router

--- src/Illustration.tsx
+++ src/Illustration.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import MainLayout from "./components/MainLayout";
 import DigitalIllustrations from "./components/DigitalIllustrations";
 import AbstractIllustrations from "./components/AbstractIllustrations";
 import illuBiene from "../src/assets/images/Biene.webp";
 
 const Illustration = () => {
   return (
-    <>
+    <MainLayout>
       <section className="md:col-start-2 md:col-span-1 max-md:px-4 text-lg tracking-wider">
         <h1 className="uppercase tracking-widest text-2xl mb-4">
           Illustrationen aus Ladbergen, die die Fantasie beflügeln
@@ -39,14 +40,17 @@ const Illustration = () => {
       </div>
       <DigitalIllustrations />
       <div className="md:col-start-3 md:col-span-1 max-md:px-4 text-lg tracking-wider">
-        <h2 className="uppercase tracking-widest text-2xl mb-4">Mein Skizzenbuch ist immer dabei</h2>
+        <h2 className="uppercase tracking-widest text-2xl mb-4">
+          Mein Skizzenbuch ist immer dabei
+        </h2>
         <p>
-          In diesem teste ich jede Menge Farben und Zeichentechniken, wenn die Ergebnisse dann auch schon gut genug sind, dann
-          werden die "Versuche" auch sofort eingescannt und zu vollwertigen Illustrationen.
+          In diesem teste ich jede Menge Farben und Zeichentechniken, wenn die
+          Ergebnisse dann auch schon gut genug sind, dann werden die "Versuche"
+          auch sofort eingescannt und zu vollwertigen Illustrationen.
         </p>
       </div>
       <AbstractIllustrations />
-    </>
+    </MainLayout>
   );
 };
 
