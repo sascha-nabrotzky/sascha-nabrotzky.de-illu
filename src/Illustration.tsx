58a01f4e4e13ea882429a23092edc9e94b389ee1
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2023-10-30 18:47:38 +0100

    add new text

--- src/Illustration.tsx
+++ src/Illustration.tsx
@@ -47,20 +47,21 @@ function Illustration() {
             Illustrationen aus Ladbergen
           </h2>
           <h1 className="uppercase tracking-widest text-2xl mb-4 font-bold">
-            Willkommen in meiner skizzenartigen Welt!
+            Was ich in meiner bunten Welt so mache:
           </h1>
           <p className="font-serif">
             Ich bin ein Illustrator, der sich auf die Verwendung von
-            Wasser-/Gouache-Farben, Bunt- und Aquarellstiften spezialisiert hat
-            und es liebt, die ursprüngliche Skizze beizubehalten, aber auch
-            komplett digital arbeitet, wenn die Zeichnung weiterverabeitet
-            werden soll.
-            <br />
-            Tauchen Sie ein in meine farbenfrohe Welt mit meinem einzigartigen
-            Stil und lassen Sie sich von meinen Illustrationen verzaubern. Ich
-            hoffe, dass Sie genauso viel Freude daran haben, meine Arbeiten zu
-            entdecken, wie ich sie beim Erstellen hatte.
+            Wasserfarben und Stiften spezialisiert hat und es liebt, die
+            ursprüngliche Skizze beizubehalten, aber auch
+            <strong> komplett digital</strong> arbeitet, wenn die Zeichnung für
+            Kinderbücher oder Werbemittel weiterverabeitet werden soll.
           </p>
+          <ul className="mt-4">
+            <li className="mb-2">Kinder-Illustration</li>
+            <li className="mb-2">Cover-Illustration</li>
+            <li className="mb-2">Kinderbücher</li>
+            <li className="mb-2">Logodesign</li>
+          </ul>
         </section>
         <div className="md:col-start-3 md:col-span-2">
           <div className="flex justify-center">
@@ -74,16 +75,11 @@ function Illustration() {
         </div>
         <ChildrensIllustrations />
         <div className="md:col-start-3 md:col-span-1 max-md:px-4 text-lg tracking-wider">
-          <h2 className="uppercase tracking-widest text-2xl mb-4 font-bold">
-            Bereiche
-          </h2>
           <p className="font-serif">
-            Meine hauptsächlichen Bereiche sind{" "}
-            <strong>Kinderbücher, Cover- und Editorial-Illustration</strong> in
-            denen ich meine kreative Ader voll ausleben kann. Im Bereich
-            Kinderbücher habe ich bereits einige eigene Projekte umgesetzt und
-            konnte auch schon für eine Kindertagespflegeeinrichtung
-            illustrieren.
+            Tauchen Sie ein in meine farbenfrohe Welt mit meinem einzigartigen
+            Stil und lassen Sie sich von meinen Illustrationen verzaubern. Ich
+            hoffe, dass Sie genauso viel Freude daran haben, meine Arbeiten zu
+            entdecken, wie ich sie beim Erstellen hatte.
           </p>
         </div>
         <DigitalIllustrations />
