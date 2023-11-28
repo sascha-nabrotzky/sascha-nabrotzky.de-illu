167b7cc7a70339d24aa07f7c80462212c424cf1e
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2023-11-28 08:21:11 +0100

    add robots txt and keys to map

--- src/components/SketchesLayout.tsx
+++ src/components/SketchesLayout.tsx
@@ -10,8 +10,8 @@ function sketchesLayout({
   return (
     <section className="md:col-start-2 md:col-span-2 grid sm:grid-cols-2 lg:grid-cols-3 gap-4 p-8 bg-white rounded">
       <h4 className="sm:col-span-2 lg:col-span-3 text-xl">{title}</h4>
-      {imgs.map((img) => (
-        <div className="flex aspect-square overflow-hidden">
+      {imgs.map((img, index) => (
+        <div className="flex aspect-square overflow-hidden" key={index}>
           <img className="w-full object-cover" src={img.src} alt={img.alt} />
         </div>
       ))}
